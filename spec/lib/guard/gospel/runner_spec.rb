4f4be423e87e1a5dea456e200fba0d449412df2c
isago <yoshihiro.isago@lasi.co.jp>
2014-10-16 15:49:50 +0900

    first commit.

---
+++ spec/lib/guard/gospel/runner_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Guard::GoSpel::Runner do
+  describe '.initialize' do
+    context 'with custom options' do
+      it 'instanciates inspector via Inspectors::Factory with custom options' do
+      end
+    end
+  end
+end
