cbba7dbbe2049986f3d0eff253eae782420acd09
isago <yoshihiro.isago@lasi.co.jp>
2014-10-20 13:27:01 +0900

    added childprocess

--- spec/lib/guard/gospel/runner_spec.rb
+++ spec/lib/guard/gospel/runner_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe Guard::GoSpel::Runner do
-  describe '.initialize' do
-    context 'with custom options' do
-      it 'instanciates inspector via Inspectors::Factory with custom options' do
-      end
+  describe '.run' do
+    let(:sut){Guard::GoSpel::Runner.new()}
+    it '.run' do
+      sut.run
+      expect(sut.pid).to_not eq(0)
+    end
+    it '.run child process' do
+      expect(ChildProcess).to receive(build).with('ls')
+      sut.run
     end
   end
 end
