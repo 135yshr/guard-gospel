2e381b6b9ce4918c1af087e59b179d24ed79c5c9
135yshr <isago@praisefirst.co.jp>
2014-10-21 23:05:35 +0900

    modified test code

--- spec/lib/guard/gospel/runner_spec.rb
+++ spec/lib/guard/gospel/runner_spec.rb
@@ -2,32 +2,35 @@ require 'spec_helper'
 
 describe Guard::GoSpel::Runner do
   let(:default_options) { Guard::GoSpel::Options::DEFAULTS }
-  let(:sut){ Guard::GoSpel::Runner.new(default_options) }
+  let(:guard_runner){ Guard::GoSpel::Runner.new(default_options) }
   describe '.run' do
-    let(:proc){ ChildProcess.build(default_options[:cmd], 'test') }
     context 'when get pid not zero' do
-      it {expect(sut.pid).to_not eq(0)}
+      it do
+        sut = Guard::GoSpel::Runner.new(default_options)
+        expect(sut.pid).to eq(nil)
+      end
     end
 
     context 'when call childprocess.build' do
+      let(:proc){ ChildProcess.build(default_options[:cmd], 'test') }
       it do
         expect(ChildProcess).to receive(:build).with(default_options[:cmd], 'test').and_return(proc)
         expect(proc).to receive(:start).once
         expect(proc).to receive(:pid).once
-        sut.run
+        guard_runner.run
       end
     end
   end
 
   describe '.ps_go_pid' do
     context 'when get empty process list' do
-      it { expect(sut.ps_go_pid).to eq([]) }
+      it { expect(guard_runner.ps_go_pid).to eq([]) }
     end
 
     context 'when get process list' do
       it do
-        sut.pid = 1
-        expect(sut.ps_go_pid).to_not eq([])
+        guard_runner.pid = 1
+        expect(guard_runner.ps_go_pid).to_not eq([])
       end
     end
   end
