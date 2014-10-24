581cae0140d9db388e4d5698bbe7e67e66cf5ea1
isago <yoshihiro.isago@lasi.co.jp>
2014-10-24 17:51:07 +0900

    refactoring test code.

--- spec/lib/guard/gospel/runner_spec.rb
+++ spec/lib/guard/gospel/runner_spec.rb
@@ -5,8 +5,9 @@ describe Guard::Gospel::Runner do
   let(:guard_runner){ Guard::Gospel::Runner.new(default_options) }
 
   describe '#run' do
+    let(:proc){ ChildProcess.build(default_options[:cmd], 'test') }
+
     context 'when success command' do
-      let(:proc){ ChildProcess.build(default_options[:cmd], 'test') }
       it 'return 0' do
         allow(proc).to receive(:exit_code) {0}
         expect(ChildProcess).to receive(:build).with(default_options[:cmd], 'test') { proc }
@@ -18,12 +19,8 @@ describe Guard::Gospel::Runner do
     end
 
     context 'when fail command' do
-      let(:proc){ ChildProcess.build(default_options[:cmd], 'test') }
       it "return 1" do
         allow(proc).to receive(:exit_code) {1}
-        expect(ChildProcess).to receive(:build).with(default_options[:cmd], 'test') { proc }
-        expect(proc).to receive(:start).once
-        expect(proc).to receive(:wait).once
         expect(::Guard::Notifier).to receive(:notify).with("Fail", title: default_options[:title], image: :failed, priority: 2)
         guard_runner.run
       end
